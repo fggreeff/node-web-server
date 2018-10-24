85cef207bc5054fc4955956dd7d7c3152edca2b9
Francois Greeff <francois.greeff@bglgroup.co.uk>
2018-10-24 18:03:36 +0100

    Add partials and helpers to templates

--- server.js
+++ server.js
@@ -2,22 +2,28 @@ const express = require('express')
 const hbs = require('hbs')
 
 var app = express()
-
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', text => {
+  return text.toUpperCase()
+})
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    currentYear: new Date().getFullYear(),
     welcomeMessage: 'welcome to the home page'
   })
 })
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   })
 })
 
