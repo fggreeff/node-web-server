b7493c62e087f5d74a27f90539caf3cd1f9ac65d
Francois <francois.greeff@bglgroup.co.uk>
2018-10-31 12:37:52 +0000

    Add project page (#3)

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ app.get('/about', (req, res) => {
   })
 })
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Project Page',
+    myProjects: 'Projects will show here'
+  })
+})
+
 app.get('/bad', (req, res) => {
   res.send({ errorMessage: 'unable to handle request' })
 })
