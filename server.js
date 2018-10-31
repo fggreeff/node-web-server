df9b5639beaaa30be79972a6b9708f56e419f177
Francois Greeff <francois.greeff@bglgroup.co.uk>
2018-10-31 12:18:12 +0000

    Setup start srcript and heroku port. Update readme

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const hbs = require('hbs')
 const fs = require('fs')
+const port = process.env.PORT || 4000
 
 var app = express()
 hbs.registerPartials(__dirname + '/views/partials')
@@ -50,6 +51,6 @@ app.get('/about', (req, res) => {
 app.get('/bad', (req, res) => {
   res.send({ errorMessage: 'unable to handle request' })
 })
-app.listen(4000, () => {
-  console.log('server is up on port 4000')
+app.listen(port, () => {
+  console.log(`server is up on port ${port}`)
 })
